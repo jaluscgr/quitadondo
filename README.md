c3247235ef62452055ab98a4be9bdef4c797d7e3
jaluscgr <155461626+jaluscgr@users.noreply.github.com>
2024-02-08 19:57:08 +0000

    probar con este modelo

---
+++ README.md
@@ -0,0 +1,9 @@
+# A doc Flet app
+
+An example of a minimal Flet app.
+
+To run the app:
+
+```
+flet run [app_directory]
+```
---
+++ main.py
@@ -0,0 +1,46 @@
+import flet as ft
+
+
+def main(page):
+    class AdaptiveIconButton(ft.IconButton):
+        def __init__(self, ios_icon, android_icon):
+            super().__init__()
+            self._ios_icon = ios_icon
+            self._android_icon = android_icon
+
+        def on_update(self):
+            self.icon = (
+                self._ios_icon
+                if self.page.platform == ft.PagePlatform.IOS
+                or self.page.platform == ft.PagePlatform.MACOS
+                else self._android_icon
+            )
+
+    def set_android(e):
+        page.platform = ft.PagePlatform.ANDROID
+        page.update()
+        print("New platform:", page.platform)
+
+    def set_ios(e):
+        page.platform = "ios"
+        page.update()
+        print("New platform:", page.platform)
+
+    add_icon = AdaptiveIconButton(
+        ios_icon=ft.cupertino_icons.AIRPLANE, android_icon=ft.icons.ADD
+    )
+    page.add(add_icon)
+    page.adaptive = True
+
+    page.add(
+        ft.Switch(label="Switch A", adaptive=True),
+        ft.ElevatedButton(
+            "Set Android", on_click=set_android, content=ft.Text("Set Android")
+        ),
+        ft.ElevatedButton("Set iOS", on_click=set_ios, content=ft.Text("Set iOS")),
+    )
+
+    print("Default platform:", page.platform)
+
+
+ft.app(target=main)
